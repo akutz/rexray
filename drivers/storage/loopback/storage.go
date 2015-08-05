a4947034186b177c7fec86cd9f7ffbed07b2a6d0
akutz <sakutz@gmail.com>
2015-08-04 20:26:31 -0500

    Initial Loopback Storage Adapter Commit
    This is the initial commit for the loopback storage adapter for the
    REX-ray daemon.

---
+++ drivers/storage/loopback/storage.go
@@ -0,0 +1,115 @@
+package loopback
+
+import (
+    "os"
+    "log"
+    "runtime"
+    
+    "github.com/emccode/rexray/drivers/storage"
+)
+
+const GOOS = runtime.GOOS
+
+var (
+    providerName string
+)
+
+type Driver struct {
+
+}
+
+func init() {
+    providerName = "loopback"
+    storagedriver.Register("loopback", Init)
+}
+
+func Init() (storagedriver.Driver, error) {
+    if os.Getenv("REXRAY_DEBUG") == "true" {
+        log.Println("Storage Driver Initialized: " + providerName)
+	}
+
+    driver := &Driver{}
+    
+    return driver, nil
+}
+
+// Lists the block devices that are attached to the instance
+func (driver *Driver) GetVolumeMapping() (interface{}, error) {
+    
+    if GOOS == "darwin" {
+        return driver.getVolumeMapping_darwin()
+    }
+    
+    if GOOS == "linux" {
+        return nil, nil
+    }
+    
+    return nil, nil
+}
+
+func (driver *Driver) getVolumeMapping_darwin() (interface{}, error) {
+    return nil, nil
+}
+
+// Get the local instance
+func (driver *Driver) GetInstance() (interface{}, error) {
+    return nil, nil
+}
+
+// Get all Volumes available from infrastructure and storage platform
+func (driver *Driver) GetVolume(string, string) (interface{}, error) {
+    return nil, nil
+}
+
+// Get the currently attached Volumes
+func (driver *Driver) GetVolumeAttach(string, string) (interface{}, error) {
+    return nil, nil
+}
+
+// Create a snpashot of a Volume
+func (driver *Driver) CreateSnapshot(bool, string, string, string) (interface{}, error) {
+    return nil, nil
+}
+
+// Get all Snapshots or specific Snapshots
+func (driver *Driver) GetSnapshot(string, string, string) (interface{}, error) {
+    return nil, nil
+}
+
+// Remove Snapshot
+func (driver *Driver) RemoveSnapshot(string) error {
+    return nil
+}
+
+// Create a Volume from scratch, from a Snaphot, or from another Volume
+func (driver *Driver) CreateVolume(bool, string, string, string, string, int64, int64, string) (interface{}, error) {
+    
+    // hdiutil create -megabytes 100 -type SPARSE -volname docker-000 docker-000
+    
+    return nil, nil
+}
+
+// Remove Volume
+func (driver *Driver) RemoveVolume(string) error {
+    return nil
+}
+
+// Get the next available Linux device for attaching external storage
+func (driver *Driver) GetDeviceNextAvailable() (string, error) {
+    return "", nil
+}
+
+// Attach a Volume to an Instance
+func (driver *Driver) AttachVolume(bool, string, string) (interface{}, error) {
+    return nil, nil
+}
+
+// Detach a Volume from an Instance
+func (driver *Driver) DetachVolume(bool, string, string) error {
+    return nil
+}
+
+// Copy a Snapshot to another region
+func (driver *Driver) CopySnapshot(bool, string, string, string, string, string) (interface{}, error)  {
+    return nil, nil
+}
---
+++ storage/storage_loopback.go
@@ -0,0 +1,5 @@
+// +build !exclude_loopback
+
+package storage
+
+import _ "github.com/emccode/rexray/drivers/storage/loopback"
